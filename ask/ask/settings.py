d4e610dcb02f255b59ef9d67c50d5e0724a9136d
Aleksey Klimenko <a_klimenko2@wargaming.net>
2016-11-22 17:20:40 +0300

    fix bugs

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'stapic',
+        'NAME': 'stepic',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': 'localhost',
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(blank=True)
-    rating = models.IntegerField
+    rating = models.IntegerField()
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes_set')
 
